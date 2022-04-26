b9724f7c3a1e33cd4b9b497a5ac8b1fbe89b4be9
Igor Zvyagintsev <zvyagintsevis@yandex.ru>
2022-04-26 21:44:45 +0300

    Added custom filters

--- src/test/java/tests/ApiTestsShop.java
+++ src/test/java/tests/ApiTestsShop.java
@@ -1,17 +1,33 @@
+package tests;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static listeners.CustomAllureListener.withCustomTemplates;
 import static org.hamcrest.Matchers.is;
 
 public class ApiTestsShop {
+
+    @BeforeAll
+    static void beforeAll() {
+        RestAssured.baseURI = "http://demowebshop.tricentis.com";
+        RestAssured.filters(withCustomTemplates());
+    }
+
 
     @Test
     void subscribe() {
         given()
                 .formParam("email", "test@qa.guru")
+                .log().uri()
                 .when()
-                .post("http://demowebshop.tricentis.com/subscribenewsletter")
+                .post("/subscribenewsletter")
                 .then()
+                .log().status()
+                .log().body()
                 .statusCode(200)
                 .body("Result", is("Thank you for signing up! A verification email has been sent. We appreciate your interest."))
                 .body("Success", is(true));
@@ -23,8 +39,10 @@ public class ApiTestsShop {
         given()
                 .formParam("pollAnswerId", "2")
                 .when()
-                .post("http://demowebshop.tricentis.com/poll/vote")
+                .post("/poll/vote")
                 .then()
+                .log().status()
+                .log().body()
                 .statusCode(200)
                 .body("error", is("Only registered users can vote."));
 
